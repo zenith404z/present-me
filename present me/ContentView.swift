6e75baa70b4e7113c88d5122e399764563b4897f
zenith404z <zainab.hawsa98@gmail.com>
2023-10-08 10:07:41 +0300

    Initial Commit

---
+++ present me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  present me
+//
+//  Created by zainab hawsa on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ present me/present_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  present_meApp.swift
+//  present me
+//
+//  Created by zainab hawsa on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct present_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
